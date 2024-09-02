234c0c33337d2f7a6ed09c4b5b8a72853a6fca0f
Guitt Web Code <77627405+francisguitt@users.noreply.github.com>
2024-09-02 20:15:18 -0300

    Update index.tsx

--- src/utils/Links/index.tsx
+++ src/utils/Links/index.tsx
@@ -1,6 +1,6 @@
 
 export const SocialLink = {
-    youtube: "https://www.youtube.com/@guitt-web-code",
-    instagram: "https://www.instagram.com/guittzoom/",
-    github: "https://github.com/francisguitt"
+    youtube: "https://www.youtube.com/",
+    instagram: "https://www.instagram.com/",
+    github: "https://github.com/"
 }
